a23457b88e6c47768bebf5a4bfc97522d084a626
ST10032904 <ST10032904@VCCONNECT.EDU.ZA>
2023-07-07 02:42:07 +0200

    messing around with buttons still

---
+++ js/button.js
@@ -0,0 +1,7 @@
+document.getElementById("Home").onclick = function () {
+    location.href = "www.alikhazem.github.io";
+};
+
+function homeRedirect(){
+    location.href = "https://www.alikhazem.github.io";
+}
